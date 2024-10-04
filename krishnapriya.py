29b23efafd21c19f5a759941522ae8766c14a286
krishnapriya609 <krishnapriyaar292@gmail.com>
2024-10-04 16:10:13 +0530

    Add files via upload

---
+++ krishnapriya.py
@@ -0,0 +1,69 @@
+#print("hello world")
+print( "hello \n world ")
+if 5 > 2:
+    print("five is greater than two")
+#if 5 > 2:
+    print("five greater than two")
+    if 5 > 2:
+        print("five greater than two")
+        print("five greater than two")
+        x = 5
+        y = "thomas"
+        print(x)
+        print(y)
+        x = 5
+        x = "thomas"
+        print(x)
+        x = str(3)
+        y = int(3)
+        z = float(3)
+        print(x)
+        print(y)
+        print(z)
+
+        x=5
+        y="john"
+        print(type(x))
+        print(type(y))
+
+        x=1j
+        y="john"
+        print(type(x))
+        print(type(y))
+
+        a=4
+        A="thomas"
+        print("a")
+        print("A")
+
+        x , y , z = "orange" , "Banana" , "cherry"
+        print(x , y , z)
+
+        x = y = z ="orange"
+        print(x)
+        print(y)
+        print(z)
+
+        x = " python "
+        y = " is "
+        z = " awesome "
+        print(x + y + z)
+
+
+
+    a="hello"
+    b="world"
+    c=a+b
+    print(c)
+
+    a="hello World"
+    print(a[0])
+
+    a="hello World"
+    print(a[1])
+
+    b="hello World"
+    print(b[2:5])
+
+    b="hello world"
+    print(b[:5])
