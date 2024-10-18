8b5ba263c250e8575624cad8136fc33d0739b85c
Krishnapriya <krishnapriyaar292@gmail.com>
2024-10-18 15:57:17 +0530

    Add files via upload

---
+++ basicpython3.py
@@ -0,0 +1,13 @@
+'''
+Author:Krishnapriya
+date:18-10-2024
+description:a Python program that demonstrates the usage of arithmetic, comparison, and logical operators. Perform a few operations and print the results.
+'''
+number1=int(input("Enter the first number:"))
+number2=int(input("Enter the second number:"))
+print("sum=",number1+number2 , "division=", number1/number2)
+print("number1 is greater than number2 ?:" , number1>number2)
+print("number1==number2")
+print("are number1 and number2 equal", number1==number2)
+print("logical AND:", number1>0 and number2>0)
+print("logical AND:", number1<0 or number2<0)
