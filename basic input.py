ddf2e83f11f8b203875e606095c10dfd1e089c76
Krishnapriya <krishnapriyaar292@gmail.com>
2024-10-18 14:30:52 +0530

    Add files via upload

---
+++ basic input.py
@@ -0,0 +1,10 @@
+'''
+Author: krishnapriya
+Date:18-10-2024
+description:After receiving the input, display a message that includes the user's name and age.
+'''
+name=input("enter your name:")
+
+age=input("enter your age:")
+print("Hello,",name, "you are ",age, "years old")
+
