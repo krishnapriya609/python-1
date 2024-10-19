3d140b1bff23f98ebc11dae26c22fd797311dca7
Krishnapriya <krishnapriyaar292@gmail.com>
2024-10-19 15:44:06 +0530

    Add files via upload

---
+++ concatenation.py
@@ -0,0 +1,11 @@
+first_name=input("enter your first name")
+last_name=input("enter your second name")
+name=first_name+ " " +last_name
+print(name)
+first_name_length= len(first_name)
+print(first_name_length)
+extracted_first_name=name[:first_name_length]
+print(extracted_first_name)
+
+
+
