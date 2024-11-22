8c35e035137d3c204a53add55317809408fe72a5
Krishnapriya <krishnapriyaar292@gmail.com>
2024-11-22 15:34:12 +0530

    Add files via upload

---
+++ module3.task2.py
@@ -0,0 +1,21 @@
+#increasing triangle
+#star=int(input("enter the number of rows"))
+#for i in range(star):
+    #for j in range(i+1):
+       # print("*",end=" ")
+    #print(" ")
+
+#decreasing triangle
+star=int(input("enter the number of rows"))
+for i in range(star,0,-1):
+    for j in range(i,0,-1):
+        print("*",end=" ")
+    print(" ")
+'''
+#hill pattern
+star=int(input('enter the number of rows'))
+for i in range(star,2):
+    for j in range(star):
+        print("*", end=" ")
+    print(" ")
+'''
