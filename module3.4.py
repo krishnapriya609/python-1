3cdd731eb34a7a16a83a21c9f6f5c19226dcf601
Krishnapriya <krishnapriyaar292@gmail.com>
2024-11-29 15:29:45 +0530

    Add files via upload

---
+++ module3.4.py
@@ -0,0 +1,13 @@
+'''
+author:Krishnapriya
+Date:29/11/2024
+'''
+def factorial(num):
+    if num==0:
+        return 1
+    else:
+        return(num*factorial(num-1))
+number=int(input("Enter the number"))
+custom=factorial(number)
+print("The factorial of number",custom)
+
